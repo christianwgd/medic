996c3a126be9bfa1483d138a06ad9f66b8a3c229
Christian Wiegand <christian.wiegand@fiduciagad.de>
2019-04-23 10:56:44 +0200

    Removed unused code

--- werte/models.py
+++ werte/models.py
@@ -6,6 +6,7 @@ from builtins import object
 from django.db import models
 from django.contrib.auth.models import User
 from django.utils.formats import date_format
+from django.utils.translation import ugettext_lazy as _
 
 
 class Wert (models.Model):
@@ -15,14 +16,13 @@ class Wert (models.Model):
         verbose_name_plural = "Messwerte"
 
     def __str__(self):
-        return ('{date}'.format(date_format(self.date)))
+        return ('{date}'.format(date=date_format(self.date)))
 
     date = models.DateTimeField(db_index=True, verbose_name='Datum', auto_now_add=True)
-    # date = models.DateTimeField(verbose_name='Datum')
     rrsys = models.DecimalField(verbose_name='RR-Sys', max_digits=3, decimal_places=0, null=True, blank=True)
     rrdia = models.DecimalField(verbose_name='RR-Dia', max_digits=3, decimal_places=0, null=True, blank=True)
     puls = models.DecimalField(verbose_name='Puls', max_digits=3, decimal_places=0, null=True, blank=True)
     temp = models.DecimalField(verbose_name='Temp', max_digits=3, decimal_places=1, null=True, blank=True)
     gew = models.DecimalField(verbose_name='Gewicht', max_digits=3, decimal_places=1, null=True, blank=True)
     bemerkung = models.CharField(verbose_name='Besonderheiten', max_length=50, null=True, blank=True)
-    ref_usr = models.ForeignKey(User, on_delete=models.PROTECT)
+    ref_usr = models.ForeignKey(User, on_delete=models.PROTECT, verbose_name=_('user'))
