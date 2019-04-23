ce269150a12011eec402f79f3bd807c4f6a01c01
Christian Wiegand <christian.wiegand@fiduciagad.de>
2019-04-23 09:10:05 +0200

    create gitignore

--- werte/forms.py
+++ werte/forms.py
@@ -7,9 +7,26 @@ from django.utils import timezone
 from werte.models import Wert
 
 
+def get_min_date():
+    try:
+        min = minval['date__min'].strftime("%Y-%m-%d")
+    except:
+        min = timezone.now().strftime("%Y-%m-%d")
+        import traceback
+        traceback.print_exc()
+    return min
+
+
+def get_min_val():
+    try:
+        min = Wert.objects.all().aggregate(Min('date'))
+    except:
+        min = None
+    return min
+
 class TimeForm(forms.Form):
-    minval = Wert.objects.all().aggregate(Min('date'))
-    minDate = minval['date__min'].strftime("%Y-%m-%d")
+    minval = get_min_val()
+    minDate = get_min_date()
     maxDate = timezone.now().strftime("%Y-%m-%d")
     vonDate = forms.DateField(
         label='von',
