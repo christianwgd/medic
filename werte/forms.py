7624f55f85bdbc58a1bdac0afc16df6f6fa5c52b
Christian Wiegand <christian.wiegand@fiduciagad.de>
2019-04-23 10:55:38 +0200

    min val removed

--- werte/forms.py
+++ werte/forms.py
@@ -7,32 +7,12 @@ from django.utils import timezone
 from werte.models import Wert
 
 
-def get_min_date():
-    try:
-        min = minval['date__min'].strftime("%Y-%m-%d")
-    except:
-        min = timezone.now().strftime("%Y-%m-%d")
-        import traceback
-        traceback.print_exc()
-    return min
-
-
-def get_min_val():
-    try:
-        min = Wert.objects.all().aggregate(Min('date'))
-    except:
-        min = None
-    return min
-
 class TimeForm(forms.Form):
-    minval = get_min_val()
-    minDate = get_min_date()
-    maxDate = timezone.now().strftime("%Y-%m-%d")
     vonDate = forms.DateField(
         label='von',
         widget=DatePickerInput(options={
           "format": "DD.MM.YYYY",
-          "minDate": minDate,
+          #"minDate": get_min_date(),
           "locale": "de"
         })
     )
@@ -40,7 +20,7 @@ class TimeForm(forms.Form):
         label='bis',
         widget=DatePickerInput(options={
           "format": "DD.MM.YYYY",
-          "maxDate": maxDate,
+          "maxDate": timezone.now().strftime("%Y-%m-%d"),
           "locale": "de"
         })
     )
