3f55e92d6581d0b8eac74d7e837e20471c781cdb
Christian Wiegand <christianwgd@users.noreply.github.com>
2022-07-17 00:05:52 +0200

    Dynamic measurement form and template

--- werte/forms.py
+++ werte/forms.py
@@ -1,37 +1,34 @@
 # -*- coding: utf-8 -*-
-from bootstrap_datepicker_plus.widgets import DatePickerInput
 from bootstrap_modal_forms.forms import BSModalModelForm
 from django import forms
-from django.core.exceptions import ValidationError
-from django.utils.translation import gettext_lazy as _
 
-from werte.models import Wert
+from werte.models import ValueType, Measurement
 
 
 class MesswertForm(BSModalModelForm):
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
-        self.fields['gew'].localize = True
-        self.fields['temp'].localize = True
+        self.Meta.fields = []
+        for idx, value_type in enumerate(ValueType.objects.active()):
+            self.fields[value_type.slug] = forms.DecimalField(
+                label=value_type.name,
+                max_digits=5,
+                decimal_places=2,
+                required=False
+            )
+            self.fields[value_type.slug].localize = True
+            if idx == 0:
+                self.fields[value_type.slug].widget = forms.NumberInput(
+                    attrs={'min': 0, "autofocus": "autofocus"}
+                )
+            else:
+                self.fields[value_type.slug].widget = forms.NumberInput(
+                    attrs={'min': 0}
+                )
+            self.Meta.fields.append(value_type.slug)
+        self.Meta.fields.append('comment')
 
     class Meta:
-        model = Wert
-        fields = ['rrsys', 'rrdia', 'puls', 'temp', 'gew', 'bemerkung']
-        widgets = {
-            'rrsys': forms.NumberInput(attrs={'min': 0, "autofocus": "autofocus"}),
-            'rrdia': forms.NumberInput(attrs={'min': 0}),
-            'puls': forms.NumberInput(attrs={'min': 0}),
-            'temp': forms.NumberInput(attrs={'min': 0}),
-            'gew': forms.NumberInput(attrs={'min': 0}),
-        }
-
-    def clean(self):
-        cleaned_data = super().clean()
-        if (
-            not cleaned_data['rrsys'] and not cleaned_data['rrdia'] and
-            not cleaned_data['puls'] and not cleaned_data['temp'] and
-            not cleaned_data['gew']
-        ):
-            raise ValidationError(_('No values entered.'), code='no_values')
-        return cleaned_data
+        model = Measurement
+        fields = ['comment']
