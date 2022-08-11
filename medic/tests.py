5733ec1148c1fb0c04816ede34334c8589654155
Christian Wiegand <christianwgd@users.noreply.github.com>
2022-08-11 10:47:30 +0200

    Medic tests improvements

--- medic/tests.py
+++ medic/tests.py
@@ -17,19 +17,32 @@ class MedicViewTest(TestCase):
         )
 
     def test_index_view_anonymous(self):
+        index_url = reverse('index')
+        response = self.client.get(index_url)
+        self.assertEqual(response.status_code, 302)
+        self.assertEqual(response.url, f'/accounts/login/?next={index_url}')
+
+    def test_index_view(self):
+        self.client.force_login(self.user)
+        index_url = reverse('index')
+        response = self.client.get(index_url)
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.templates[0].name, "index.html")
+
+    def test_start_view_anonymous(self):
         start_url = reverse('startpage')
         response = self.client.get(start_url)
         self.assertEqual(response.status_code, 302)
         self.assertEqual(response.url, f'/accounts/login/?next={start_url}')
 
-    def test_index_view_no_start_page(self):
+    def test_start_view_no_start_page(self):
         self.client.force_login(self.user)
         start_url = reverse('startpage')
         response = self.client.get(start_url)
         self.assertEqual(response.status_code, 302)
         self.assertEqual(response.url, reverse('index'))
 
-    def test_index_view_startpage_set(self):
+    def test_start_view_startpage_set(self):
         profile = self.user.profile
         profile.my_start_page = StartUrl.objects.create(
             name='measurements list',
@@ -42,7 +55,7 @@ class MedicViewTest(TestCase):
         self.assertEqual(response.status_code, 302)
         self.assertEqual(response.url, reverse(profile.my_start_page.url))
 
-    def test_logoff_view(self):
+    def test_start_view(self):
         self.client.force_login(self.user)
         logoff_url = reverse('logoff')
         response = self.client.post(logoff_url)
