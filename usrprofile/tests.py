2a9203d410832bd8713d84baef5bf09498013557
Christian Wiegand <christianwgd@users.noreply.github.com>
2022-08-11 13:49:21 +0200

    Remove unused import

--- usrprofile/tests.py
+++ usrprofile/tests.py
@@ -1,5 +1,4 @@
 from django.contrib import auth
-from django.core.exceptions import ValidationError
 from django.test import TestCase
 from django.urls import reverse
 from django.utils import formats
