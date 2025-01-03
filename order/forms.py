2ab8c0fb4ade2dcaf30c23cbf446ed4d00ac084a
Christian Wiegand <christianwgd@users.noreply.github.com>
2025-01-03 16:14:19 +0100

    Fix expired meds in orders

--- order/forms.py
+++ order/forms.py
@@ -3,6 +3,7 @@ from django import forms
 
 from medicament.models import Medicament
 from order.models import Order
+from prescription.models import Prescription
 
 
 class OrderForm(BSModalModelForm):
@@ -15,6 +16,7 @@ class OrderForm(BSModalModelForm):
         user = kwargs.pop('user', None)
         super().__init__(*args, **kwargs)
         self.fields['medicaments'].widget = forms.CheckboxSelectMultiple()
+        prescriptions = Prescription.objects.active(for_user=user)
         self.fields['medicaments'].queryset = Medicament.objects.filter(
-            id__in=user.prescriptions.values_list('medicament__id', flat=True),
+            id__in=prescriptions.values_list('medicament__id', flat=True),
         )
