a40fcd560011ecd27aa407d39afa38c0166bf19b
Christian Wiegand <christianwgd@users.noreply.github.com>
2025-08-14 15:45:30 +0200

    Remove unused logout view, fix tests

--- medic/views.py
+++ medic/views.py
@@ -1,7 +1,6 @@
 from logging import getLogger
 
 from django.contrib import auth
-from django.contrib.auth import logout
 from django.contrib.auth.decorators import login_required
 from django.shortcuts import render, redirect
 from django.urls import reverse_lazy
